cb95ec729ed5dd35e19e6ac479e620dd62295405
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-07 15:36:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,17 @@
 #write your code here
+require time
 
 def countdown n
+  until n == 0
+    puts "#{n} SECOND(S)!"
+    n -= 1
+    sleep 1  
+  end
+  "HAPPY NEW YEAR!"
+end
+
+
+def countdown_with_sleep n
   until n == 0
     puts "#{n} SECOND(S)!"
     n -= 1
